80485d75058e931e4e0818cc696f42be8a30f3dd
Jess <d1al.t0ne@outlook.com>
2022-02-03 09:33:26 -0500

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -7,8 +7,21 @@ brew update
 brew upgrade
 
 # Utilities
+brew install findutils
+# commands have been installed with the prefix "g".
+# To use these commands with their normal names.
+# add a "gnubin" directory to your PATH from your bashrc
+# PATH="$(brew --prefix)/opt/findutils/libexec/gnubin:$PATH"
+
+# Insall Updated Bash
 brew install bash
 brew install bash-completion2
+
+# Make Brew Bash as default
+if ! fgrep -q "/usr/local/bin/bash" /etc/shells; then
+  echo "/usr/local/bin/bash" | sudo tee -a /etc/shells;
+  chsh -s "/usr/local/bin/bash";
+fi;
 
 # Install more recent versions of some macOS tools.
 brew install screen
